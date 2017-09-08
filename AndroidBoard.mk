a2a7860ff64f4f4076b80b5147bde09b08ea388c
ASAZING <malcolmedina206@gmail.com>
2017-09-08 11:43:21 -0500

    Update wifi firmware file config (use symlinks)

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -31,3 +31,12 @@ $(shell mkdir -p $(TARGET_OUT)/vendor/lib; \
 # Hack for build
 $(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libminui_intermediates)
 $(shell touch $(OUT)/obj/SHARED_LIBRARIES/libminui_intermediates/export_includes)
+
+# Create a link for the WCNSS config file, which ends up as a writable
+# version in /data/misc/wifi
+
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
+    ln -sf /system/etc/wifi/WCNSS_qcom_wlan_nv.bin \
+    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
+    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
+    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
